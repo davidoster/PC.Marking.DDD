81cb5dfabe402e1822b10c61f8536b0212772276
Pasparakis, George <George.Pasparakis@peoplecert.org>
2023-04-07 13:42:54 +0300

    Add project files.

---
+++ Domain/Exam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Exam: GenericExam
+    {
+        List<Question> Questions; // Q1
+        List<PossibleAnswers> Answers;  // Q1.PossibleAnswers[0] = "sdfsdfsdf", Q1.PossibleAnswers[1] = "sdfsdfsdf"
+        List<SubmittedAnswers> SubmittedAnswers;
+    }
+}
---
+++ Domain/RepoInterfaces/IExam.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoInterfaces
+{
+    internal interface IExam
+    {
+        /// <summary>
+        /// Gets all the exams of a specific marker
+        /// </summary>
+        /// <returns></returns>
+        List<Exam> GetExamList();
+
+        List<Exam> GetUnmarkedExamList();
+        List<Exam> GetMarkedExamList();
+
+        /// <summary>
+        /// what is the output of the marking process????????
+        /// </summary>
+        /// <param name="examId"></param>
+        /// <returns></returns>
+        double MarkExam(int examId);
+    }
+}
