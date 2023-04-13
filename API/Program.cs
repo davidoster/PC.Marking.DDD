a1636af23891c7e638a56c74aa8069d8dc06349e
Avi-Karalis <averkkaralis@gmail.com>
2023-04-13 11:29:10 +0300

    API WIP

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Infrastructure.DependencyResolver;
+
 namespace API {
     public class Program {
         public static void Main(string[] args) {
@@ -9,6 +11,8 @@ namespace API {
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddInfrastructure();
 
             var app = builder.Build();
 
--- Infrastructure/DependencyResolver/ServiceCollectionExtensions.cs
+++ Infrastructure/DependencyResolver/ServiceCollectionExtensions.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Domain.DependencyResolver;
+using Microsoft.Extensions.DependencyInjection;
+using RepoInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.DependencyResolver {
-    internal class ServiceCollectionExtensions {
+    public static class ServiceCollectionExtensions {
+       
+        public static void AddInfrastructure (this IServiceCollection services) {
+            services.AddDatabaseContext();
+
+            services.AddScoped<IExamRepository, ExamRepository>();
+            services.AddScoped<ISectionRepository, SectionRepository>();
+            services.AddScoped<IQuestionRepository, QuestionRepository>();
+
+        }
+
     }
 }
