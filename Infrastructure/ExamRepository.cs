e166b0c1411066463116403cb65d021505568e64
VickyBisbiki <vicksbiz@gmail.com>
2023-04-13 13:27:29 +0300

    Repository Exam Vicks

--- Infrastructure/ExamRepository.cs
+++ Infrastructure/ExamRepository.cs
@@ -3,41 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApplicationDbContext;
 using Domain;
+using Microsoft.EntityFrameworkCore;
 using RepoInterfaces;
 
 namespace Infrastructure {
     public class ExamRepository : IExamRepository {
-        public Task Delete(int Id) {
-            throw new NotImplementedException();
+
+        private readonly MarkingDbContext _context;
+        public ExamRepository(MarkingDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Delete(int Id) {
+
+             _context.Exams.Remove(await GetById(Id));
+            await Save();
         }
 
-        public Task<List<Exam>> GetAll() {
-            throw new NotImplementedException();
+        public async Task<List<Exam>> GetAll() {
+
+            return await _context.Exams.ToListAsync();
         }
 
-        public Task<Exam> GetById(int Id) {
-            throw new NotImplementedException();
+        public async Task<Exam> GetById(int Id) {
+
+           return await _context.Exams.FirstOrDefaultAsync(x => x.Id == Id);
         }
 
-        public Task<List<Exam>> GetMarkedList() {
-            throw new NotImplementedException();
+        public async Task<List<Exam>> GetMarkedList() {
+
+            return await _context.Exams.Where(x => x.MarkingState == MarkingState.AutoMarked).ToListAsync();
         }
 
-        public Task<List<Exam>> GetUnmarkedList() {
-            throw new NotImplementedException();
+        public async Task<List<Exam>> GetUnmarkedList() {
+
+            return await _context.Exams.Where(x => x.MarkingState == MarkingState.UnMarked).ToListAsync();
         }
 
-        public Task<Exam> Insert() {
-            throw new NotImplementedException();
+        public async Task<Exam> Insert(Exam exam) {
+
+            await _context.Exams.AddAsync(exam);
+            await Save();
+            return exam;
         }
 
-        public Task Save() {
-            throw new NotImplementedException();
+        public async Task Save() {
+            
+            await _context.SaveChangesAsync();
+
         }
 
-        public Task<Exam> Update(Exam entity) {
-            throw new NotImplementedException();
+        public async Task<Exam> Update(Exam entity) {
+           
+            _context.Exams.Update(entity);
+            await Save();
+            return entity;
+
         }
     }
 }
