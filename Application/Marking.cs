c67e411580ad288efc8ba47911a5072483e386e6
IoaMertz <119110449+IoaMertz@users.noreply.github.com>
2023-04-13 15:42:27 +0300

    marking service

--- Application/Marking.cs
+++ Application/Marking.cs
@@ -13,11 +13,18 @@ namespace Application {
 
         public float MarkingService(Exam exam) 
         {
+            // 1st 
             exam.Sections.ForEach(section => { section.Questions.ForEach(q => QuestionMarkingBase.QuestionMarkingService(q));});
 
             exam.Sections.ForEach(section => SectionMarkingBase.SectionMarkingService(section));
 
             ExamMarkingBase.ExamAutoMarkingService(exam);
+
+            //
+
+            //var tasks = new List<Task>();
+            //exam.Sections.ForEach(section => section.Questions.ForEach(q => tasks.Add(Task.Run(()=> QuestionMarkingBase.QuestionMarkingService(q)))));
+            //Task.WaitAll(tasks.ToArray());
 
             return (float)exam.OverallExamScore;
            
